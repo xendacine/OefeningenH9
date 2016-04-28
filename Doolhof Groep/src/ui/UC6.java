10973347b85b70a6d37fd486291841b1ed5406a6
Sven V <Sven V@192.168.183.2>
2016-04-28 09:31:00 +0200

    Doelkaarten worden verdeeld + test in uc2 of ze wel degelijk uniek zijn

---
+++ Doolhof Groep/src/ui/UC6.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+import domein.DomeinController;
+import domein.Taal;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sven V
+ */
+public class UC6 {
+    private final DomeinController dc;
+     private final DoolhofApp app;
+     private Taal taal;
+     ResourceBundle tekst;   
+     private final Scanner scanner;
+    
+    
+    public UC6(DomeinController dc,DoolhofApp app,Taal taal)
+    {
+        this.scanner = new Scanner(System.in);
+        this.dc = dc;
+        this.app = app;
+        this.taal = taal;
+        
+    }
+    
+    public void bewaarSpel(){
+        System.out.println("Geef een naam voor het spel:");
+         String spelnaam = scanner.next();
+        //if(spelnaam= 8)  {
+            
+     //   }      
+    }
+}
